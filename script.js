30db6bbfd7efadc6b8238214f30e4b7203d7dd40
chrstinalin <44586776+chrstinalin@users.noreply.github.com>
2023-05-10 14:49:50 -0400

    Frontend UI

---
+++ script.js
@@ -0,0 +1,20 @@
+
+// Title Generation
+const kaomoji = ["(─‿‿─)♡" , "(´｡• ᵕ •｡`) ♡" , "(*♡∀♡)" , "(｡・//ε//・｡)" , "(´ ω `♡)" , "♡( ◡‿◡ )" , "(◕‿◕)♡" , "♡ (￣З￣)" , "(ღ˘⌣˘ღ)" , "(♡°▽°♡)" , "♡(｡- ω -)" , "♡ ～('▽^人)" , "(´• ω •`) ♡" , "(´ ε ` )♡" , "(´｡• ω •｡`) ♡" , "( ´ ▽ ` ).｡ｏ♡" , "╰(*´︶`*)╯♡" , "(*˘︶˘*).｡.:*♡" , "(♡˙︶˙♡)" , "♡＼(￣▽￣)／♡"];
+const title = document.querySelector('#title');
+title.textContent = `PXL ${kaomoji[Math.floor(Math.random() * 20)]}`
+
+// Button Action
+const buttons = document.querySelectorAll('.buttons button');
+
+buttons.forEach( item => {
+
+    item.addEventListener('mouseenter', function(){
+        item.classList.add('mouseover');
+    })
+
+    item.addEventListener('mouseleave', function(){
+        item.classList.remove('mouseover');
+    })
+
+})
