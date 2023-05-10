38bb91fe5de4637843cb0700008f73bec14ce3de
chrstinalin <44586776+chrstinalin@users.noreply.github.com>
2023-05-10 15:36:14 -0400

    testing build

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 // Title Generation
-const kaomoji = ["(─‿‿─)♡" , "(´｡• ᵕ •｡`) ♡" , "(*♡∀♡)" , "(｡・//ε//・｡)" , "(´ ω `♡)" , "♡( ◡‿◡ )" , "(◕‿◕)♡" , "♡ (￣З￣)" , "(ღ˘⌣˘ღ)" , "(♡°▽°♡)" , "♡(｡- ω -)" , "♡ ～('▽^人)" , "(´• ω •`) ♡" , "(´ ε ` )♡" , "(´｡• ω •｡`) ♡" , "( ´ ▽ ` ).｡ｏ♡" , "╰(*´︶`*)╯♡" , "(*˘︶˘*).｡.:*♡" , "(♡˙︶˙♡)" , "♡＼(￣▽￣)／♡"];
-const title = document.querySelector('#title');
-title.textContent = `PXL ${kaomoji[Math.floor(Math.random() * 20)]}`
+//const kaomoji = ["(─‿‿─)♡" , "(´｡• ᵕ •｡`) ♡" , "(*♡∀♡)" , "(｡・//ε//・｡)" , "(´ ω `♡)" , "♡( ◡‿◡ )" , "(◕‿◕)♡" , "♡ (￣З￣)" , "(ღ˘⌣˘ღ)" , "(♡°▽°♡)" , "♡(｡- ω -)" , "♡ ～('▽^人)" , "(´• ω •`) ♡" , "(´ ε ` )♡" , "(´｡• ω •｡`) ♡" , "( ´ ▽ ` ).｡ｏ♡" , "╰(*´︶`*)╯♡" , "(*˘︶˘*).｡.:*♡" , "(♡˙︶˙♡)" , "♡＼(￣▽￣)／♡"];
+//const title = document.querySelector('#title');
+//title.textContent = `PXL ${kaomoji[Math.floor(Math.random() * 20)]}`
 
 // Button Action
 const buttons = document.querySelectorAll('.buttons button');
@@ -10,10 +10,12 @@ const buttons = document.querySelectorAll('.buttons button');
 buttons.forEach( item => {
 
     item.addEventListener('click', function(){
-        if(item.classList.contains('select')) item.classList.remove('select');
-        else{
-            buttons.forEach(item => item.classList.remove('select'))
-            item.classList.add('select');
+        if(item.id != 'color'){
+            if(item.classList.contains('select')) item.classList.remove('select');
+            else{
+                buttons.forEach(item => item.classList.remove('select'))
+                item.classList.add('select');
+            }
         }
     })
 
